1e7564f5c99d2108abb229e7a8f3496cd96c56df
rogerogers <rogers@rogerogers.com>
2024-07-16 23:23:05 +0800

    chore: new home
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://blog.rogerogers.com",
   integrations: [
     mdx(),
     sitemap(),
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Rogerogers";
+export const SITE_DESCRIPTION =
+  "Explore Full Stack Development & DevOps insights, plus open-source engagement. Blend tech expertise with community-driven projects.";
