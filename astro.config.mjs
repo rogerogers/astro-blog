d5c90334aa00d3285cbcc65b670e8d6fbcb4e9f0
rogerogers <rogers@rogerogers.com>
2025-07-07 22:05:02 +0800

    feat: remove docs
    Signed-off-by: rogerogers <rogers@rogerogers.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import { defineConfig } from 'astro/config'
-
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
+import devtoolsJson from 'vite-plugin-devtools-json'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.rogerogers.com',
   integrations: [mdx(), sitemap()],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), devtoolsJson()],
   },
 })
--- src/content.config.ts
+++ src/content.config.ts
@@ -16,19 +16,4 @@ const blog = defineCollection({
     }),
 })
 
-const docs = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: './src/content/docs', pattern: '**/*.{md,mdx}' }),
-  // Type-check frontmatter using a schema
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      // Transform string to Date object
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-      heroImage: image().optional(),
-    }),
-})
-
-export const collections = { blog, docs }
+export const collections = { blog }
